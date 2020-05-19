b1861f9464d7a8488c5a598f8b9ad5b11d441c7e
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-19 18:52:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,7 +8,12 @@ def loop_message_five_times(string)
   end 
 end
 
-def loop_message_n_times
+def loop_message_n_times(string, n)
+  count = 0 
+  while count < n
+    puts "#{string}" 
+    count += 1 
+  end 
 end
 
 def output_array
